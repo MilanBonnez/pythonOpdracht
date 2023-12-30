6fb1c0f35b0c703830c2474273827d4d986eb013
Milan Bonnez <milan.bonnez@student.vives.be>
2023-12-30 10:58:04 +0100

    created database and manageDb.py file
    - created database.db with tables Students and Teachers.
    - created file manageDb.py: contains class Database with methods showTable, addPerson, deletePerson, showPerson

---
+++ db/ManageDb.py
@@ -0,0 +1,31 @@
+
+
+import sqlite3
+
+class Database:
+    def __init__(self, db_path):
+        self.con = sqlite3.connect(db_path)
+        self.cursor = self.con.cursor()
+
+    def showTable(self, table):
+        query = f"SELECT * FROM {table}"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        return rows
+
+    def addPerson(self, table, firstName, lastName):
+        query = f"INSERT INTO {table} (firstName, lastName) VALUES ('{firstName}', '{lastName}')"
+         self.cursor.execute(query)
+         self.con.commit()
+
+      def deletePerson(self, personId, table):
+        query = f"DELETE FROM {table} WHERE id = {personId}"
+        self.cursor.execute(query)
+        self.con.commit()
+
+
+    def showPerson(self, personId, table):
+        query = f"SELECT * FROM {table} WHERE user_id = {personId}"
+        self.cursor.execute(query)
+        person = self.cursor.fetchall()
+        return person
